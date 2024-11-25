e18630b1085dd723077803d6c8ddb6c801e6e2f6
FixingMind5 <aguilarmanu5@gmail.com>
2024-11-25 10:37:26 -0600

    Initial Commit

---
+++ Tasker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tasker
+//
+//  Created by CEDAM18 on 25/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
